e998b3bb4a6c2936abc3aec55eedee21a7757752
Elazar Levin <1504371@students.wits.ac.za>
2019-06-16 04:40:27 +0200

    neatened up code a little

--- core/src/com/lazi/yelda/Main.java
+++ core/src/com/lazi/yelda/Main.java
@@ -26,8 +26,6 @@ public class Main extends Game {
 		assetManager.finishLoading();
 	
 		atlas = new TextureAtlas("res/Overworld.atlas");
-		//sprite = atlas.createSprite("basic_grass");
-		//sprite.setPosition(10, 10);
 		screen = new GameScreen(this);
 		this.setScreen(screen);
 		
@@ -37,12 +35,7 @@ public class Main extends Game {
 	public void render () {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-	//	batch.begin();
-	//	batch.draw(img, 0, 0);
-	//	batch.end();
 		super.render();
-		//makeGrass();
-		
 	}
 	
 	@Override
@@ -50,21 +43,7 @@ public class Main extends Game {
 		batch.dispose();
 		img.dispose();
 	}
-	private void makeGrass()
-	{
-		for(int i=0;i<Gdx.graphics.getWidth()/16;i++)
-		{
-			for(int j=0;j<Gdx.graphics.getHeight()/16;j++)
-			{
-				batch.begin();
-				sprite.setPosition(i*16,j*16);
-				sprite.draw(batch);
-				batch.end();
-			}
-		}
-	
-	}
-	
+
 	public AssetManager getAssetManager() {
 		return assetManager;
 	}
