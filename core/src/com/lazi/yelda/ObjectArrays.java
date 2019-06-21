c7366b59500afa6248b8baac0c15159fbb7ddef7
Elazar Levin <1504371@students.wits.ac.za>
2019-06-21 12:06:17 +0200

    added some shit

--- core/src/com/lazi/yelda/ObjectArrays.java
+++ core/src/com/lazi/yelda/ObjectArrays.java
@@ -7,11 +7,14 @@ import com.badlogic.gdx.utils.Array;
 public class ObjectArrays {
 	public static Array<TERRAIN> walls;
 	public static Array<TERRAIN> animated;
+	public static Array<TERRAIN> canGoBehind;
+	public static int[][] map;
 	
 	public static void initiate()
 	{
 		walls=new Array<TERRAIN>();
 		animated=new Array<TERRAIN>();
+		canGoBehind =new Array<TERRAIN>();
 	}
 	
 	
